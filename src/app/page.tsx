1b66763b37e5927932853361460fa93651278170
Alex <acim650@gmail.com>
2026-04-15 12:28:09 -0700

    able to connect to blog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import { inter } from "./layout";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-cyan-400/10 backdrop-blur-xl border border-white/20 shadow-xl">
+    <div className="bg-cyan-400/10 backdrop-blur-xl border border-white/20 shadow-xl min-h-screen">
       <nav className="p-1 flex justify-end">
         <button className="p-3 cursor-pointer">
           Blog
@@ -13,44 +14,20 @@ export default function Home() {
         <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50 font-s">
           Alex Cortez
         </h1>
-        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={100}
-            height={20}
-            priority
-          />
-          <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          </div>
-          <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-            <a
-              className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={16}
-                height={16}
-              />
-              Deploy Now
-            </a>
-            <a
-              className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Projects
-            </a>
+        <main className="flex flex-col justify-center w-full max-w-3xl items-center py-32 px-16 sm:items-start">
+          <div className="flex justify-center w-full">
+            <Link href="/projects">
+              <button
+                className="flex h-12 w-full items-center justify-center rounded-full border border-solid px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px] cursor-pointer"
+              >
+                Projects
+              </button>
+            </Link>
           </div>
         </main>
       </div>
     </div>
   );
 }
+
+// Put lightning bolt of thunder for lightning to symbolize 
