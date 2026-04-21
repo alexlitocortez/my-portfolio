be1deaf803d063edb245260b0b73435d1d188366
Alex <acim650@gmail.com>
2026-04-21 13:30:04 -0700

    added linkedin icon and name in nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,45 +3,67 @@ import { inter } from "./layout";
 import Link from "next/link";
 import GearLoader from "../components/GearLoader";
 import GithubIcon from "../../public/images/github.svg";
+import LinkedIcon from "../../public/images/linkedin.svg";
 
 export default function Home() {
   return (
     <div className="bg-background backdrop-blur-xl border border-white/20 shadow-xl min-h-screen">
-      <nav className="p-1 flex justify-end">
+      <nav className="p-1 flex justify-around">
+        <div className="flex font-semibold p-3 gap-2">
+          <h3>
+            Alex
+          </h3>
+          <h3 className="bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 bg-clip-text text-transparent">
+            Cortez
+          </h3>
+        </div>
         <Link href="/blog">
           <button className="p-3 cursor-pointer">
             Blog
           </button>
         </Link>
       </nav>
-      <div className="flex flex-col flex-1 items-center justify-center">
-        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight font-s">
-          Alex
-        </h1>
-        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight bg-gradient-to-r from-blue-900 via-indigo-700 to-purple-700 bg-clip-text text-transparent">
-          Cortez
+      <div className="flex flex-1 items-center justify-center gap-2">
+        <h1 className="flex items-center justify-center gap-2 text-3xl font-semibold tracking-tight">
+          <span>Alex</span>
+          <span className="bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 bg-clip-text text-transparent">
+            Cortez
+          </span>
         </h1>
       </div>
       <div className="flex flex-col flex-1 items-center justify-center font-sans shadow-xl rounded-2xl p-8">
         <main className="flex flex-col justify-center w-full max-w-3xl items-center py-32 px-16 sm:items-start">
           <div className="flex flex-col justify-center items-center gap-4 w-full">
             <GearLoader />
-            <div className="flex justify-center p-5">
+            <div className="flex justify-center items-center gap-4 p-5 max-[600px]:flex-col">
               <Link href="/projects">
                 <button
-                  className="bg-accent flex h-12 w-full items-center justify-center rounded-full px-5 cursor-pointer"
+                  className="bg-accent flex h-12 w-full items-center justify-center rounded-full px-5 cursor-pointer hover:bg-gray"
                 >
                   Projects
                 </button>
               </Link>
               <Link href="/">
-                <Image
-                  width={100}
-                  height={100}
-                  src={GithubIcon}
-                  alt="Github icon"
-                  className="w-full h-full object-contain cursor-pointer"
-                />
+                <div className="h-12 w-12 flex items-center justify-center bg-secondary rounded-full">
+                  <Image
+                    width={5}
+                    height={5}
+                    src={GithubIcon}
+                    alt="Github icon"
+                    className="w-full object-contain cursor-pointer"
+                  />
+                </div>
+              </Link>
+              <Link href="/">
+                <div className="h-13 w-13 flex items-center justify-center bg-secondary rounded-full">
+                  <Image
+                    width={5}
+                    height={5}
+                    src={LinkedIcon}
+                    alt="Linkedin icon"
+                    className="w-full object-contain cursor-pointer"
+                  />
+                </div>
               </Link>
             </div>
           </div>
@@ -51,5 +73,4 @@ export default function Home() {
   );
 }
 
-// Fix Github icon logo
-// Add LinkedIn
+// 
